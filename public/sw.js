be70aaf1ef1cd655a537bce82acdccd9ec4dbbb7
pongdang <mirckzk32@gmail.com>
2026-09-15 09:25:36 +0900

    Step 33.1 fix member layout

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "jjinc-manager-v19";
+const CACHE_NAME = "jjinc-manager-v20";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (event) => {
