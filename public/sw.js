cb70f164885915b52d6af4ab757f383306a9fc4e
pongdang <mirckzk32@gmail.com>
2026-09-18 13:42:38 +0900

    Step 34.2.1 fix CSS build error

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "jjinc-manager-v24";
+const CACHE_NAME = "jjinc-manager-v25";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (event) => {
